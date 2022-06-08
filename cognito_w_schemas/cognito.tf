12944f560b377119103ee28d033111460b2d3fbe
bukiya0212 <bukiya0212@gmail.com>
2022-06-08 05:06:06 +0000

    属性検証方法の守勢

--- cognito_w_schemas/cognito.tf
+++ cognito_w_schemas/cognito.tf
@@ -16,17 +16,9 @@ resource "aws_cognito_user_pool" "user_pool" {
     }
   }
 
-  account_recovery_setting {
-    recovery_mechanism {
-      name     = "verified_email"
-      priority = 1
-    }
-
-    recovery_mechanism {
-      name     = "verified_phone_number"
-      priority = 2
-    }
-  }
+  auto_verified_attributes = [
+    "email",
+  ]
 
   admin_create_user_config {
     allow_admin_create_user_only = var.create_user
