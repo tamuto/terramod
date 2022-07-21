086dd0fda5b99b798ea3e41d671095d8b0f0c974
bukiya0212 <bukiya0212@gmail.com>
2022-07-21 05:54:16 +0000

    2段階認証設定追加

--- cognito_w_schemas/sms.tf
+++ cognito_w_schemas/sms.tf
@@ -10,25 +10,11 @@ data "aws_iam_policy_document" "sms_role" {
 }
 
 resource "aws_iam_role" "iam_role_mfa" {
-name = "${var.name}"
+name = "${var.name}-sms-role"
 assume_role_policy = data.aws_iam_policy_document.sms_role.json
-managed_policy_arns = [aws_iam_policy.policy_one.arn]
 }
 
-resource "aws_iam_policy" "policy_one" {
-  name = "policy-sns"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = [
-            "sns:GetTopicAttributes",
-            "sns:List*"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
+resource "aws_iam_role_policy_attachment" "aws_sns_policy_attach" {
+    role = aws_iam_role.iam_role_mfa.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonSNSReadOnlyAccess"
 }
