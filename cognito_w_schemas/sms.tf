ec17657600ca2b5c1ae5ccc39bf82e626c8d9801
bukiya0212 <bukiya0212@gmail.com>
2022-07-21 05:05:50 +0000

    2段階認証設定追加

--- cognito_w_schemas/sms.tf
+++ cognito_w_schemas/sms.tf
@@ -12,4 +12,23 @@ data "aws_iam_policy_document" "sms_role" {
 resource "aws_iam_role" "iam_role_mfa" {
 name = "${var.name}"
 assume_role_policy = data.aws_iam_policy_document.sms_role.json
+managed_policy_arns = [aws_iam_policy.policy_one.arn]
+}
+
+resource "aws_iam_policy" "policy_one" {
+  name = "policy-sns"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = [
+            "sns:GetTopicAttributes",
+            "sns:List*"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
