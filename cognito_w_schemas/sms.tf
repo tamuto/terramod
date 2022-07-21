66dfc14275883addc1073a8c250b86db4652f438
bukiya0212 <bukiya0212@gmail.com>
2022-07-21 03:05:16 +0000

    2段階認証設定追加

--- cognito_w_schemas/sms.tf
+++ cognito_w_schemas/sms.tf
@@ -11,5 +11,5 @@ data "aws_iam_policy_document" "sms_role" {
 
 resource "aws_iam_role" "iam_role_mfa" {
 name = "${var.name}"
-assume_role_policy = data.aws_iam_policy_document.lambda_assume_policy.json
+assume_role_policy = data.aws_iam_policy_document.sms_role.json
 }
