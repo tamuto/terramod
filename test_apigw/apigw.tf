6edb57801d5b54c1bdc09f10c381ffc2a11a319f
tagaku0510 <yuuki.tagaku@abi-inc.co.jp>
2022-08-23 08:12:52 +0000

    test_apigw作成

--- test_apigw/apigw.tf
+++ test_apigw/apigw.tf
@@ -1,4 +1,14 @@
 resource "aws_api_gateway_rest_api" "example" {
   name        = "EstQ_Dev_API"
   description = "example API Gateway"
+  body        = "${data.template_file.swagger.rendered}"
+}
+
+data "template_file" "swagger" {
+  template = "${file("${path.module}/openapi.json")}"
+
+  vars = {
+    title                   = "EstQ_Dev_API"
+    aws_region_name         = "ap-northeast-1"
+  }
 }
