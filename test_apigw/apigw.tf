ea8ac9f791f90c71bc6197d12b33c92430d733d8
tagaku0510 <yuuki.tagaku@abi-inc.co.jp>
2022-08-23 07:43:41 +0000

    test_apigw作成

--- test_apigw/apigw.tf
+++ test_apigw/apigw.tf
@@ -1,4 +1,10 @@
 resource "aws_api_gateway_rest_api" "example" {
-  name        = "example"
+  name        = "EstQ_Dev_API"
   description = "example API Gateway"
+}
+
+resource "aws_api_gateway_resource" "api" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+  path_part   = "api"
 }
