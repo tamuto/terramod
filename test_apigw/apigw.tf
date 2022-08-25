1b36cc65dce140219001e2dbc87c314f0349b130
tagaku0510 <yuuki.tagaku@abi-inc.co.jp>
2022-08-25 05:51:01 +0000

    test_apigw作成

--- test_apigw/apigw.tf
+++ test_apigw/apigw.tf
@@ -13,9 +13,9 @@ data "template_file" "swagger" {
   }
 }
 
-resource "aws_api_gateway_authorizer" "api_authorizer" {
-  name          = "CognitoUserPoolAuthorizer"
-  type          = "COGNITO_USER_POOLS"
-  rest_api_id   = "${aws_api_gateway_rest_api.example.id}"
-  provider_arns = [var.cognito_user_arn]
-}
+# resource "aws_api_gateway_authorizer" "api_authorizer" {
+#   name          = "CognitoUserPoolAuthorizer"
+#   type          = "COGNITO_USER_POOLS"
+#   rest_api_id   = "${aws_api_gateway_rest_api.example.id}"
+#   provider_arns = [var.cognito_user_arn]
+# }
