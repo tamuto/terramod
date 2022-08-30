45b097be3c0b0ff3e6672398152dc474e3d421dc
tagaku0510 <yuuki.tagaku@abi-inc.co.jp>
2022-08-30 01:51:16 +0000

    test_cognito作成

--- test_cognito/main.tf
+++ test_cognito/main.tf
@@ -87,7 +87,7 @@ data "aws_iam_policy_document" "example" {
   }
 }
 
-data "aws_ses_identity_policy" "example" {
+resource "aws_ses_identity_policy" "example" {
   identity = "${aws_ses_email_identity.example.arn}"
   name     = var.identity_name
   policy   = data.aws_iam_policy_document.example.json
