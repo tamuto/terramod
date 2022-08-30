b8e13c1f5e57de5a10aca3d062d80ce87bdf1bdf
tagaku0510 <yuuki.tagaku@abi-inc.co.jp>
2022-08-30 00:38:02 +0000

    test_cognito作成

---
+++ test_cognito/main.tf
@@ -0,0 +1,94 @@
+# User Pool
+resource "aws_cognito_user_pool" "user_pool" {
+  name = var.poolname
+  auto_verified_attributes = var.auto_verified
+
+  dynamic "schema" {
+    for_each = var.schemas
+    content {
+      attribute_data_type = schema.value.type
+      name = schema.key
+      required = false
+      mutable = schema.value.mutable
+      string_attribute_constraints {
+        min_length = schema.value.min_length
+        max_length = schema.value.max_length
+      }
+    }
+  }
+
+  admin_create_user_config {
+    allow_admin_create_user_only = var.create_user
+  }
+
+  username_configuration {
+    case_sensitive = false
+  }
+
+  password_policy {
+    minimum_length = 8
+    require_lowercase = true
+    require_numbers = true
+    require_symbols = true
+  }
+
+  verification_message_template {
+    email_subject = var.email_subject
+    email_message = var.email_message
+  }
+
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = var.account_recovery
+      priority = 1
+    }
+  }
+
+  mfa_configuration = var.mfa_configuration
+
+  lifecycle {
+    ignore_changes = [
+      ### AWS doesn't allow schema updates, so every build will re-create
+      ### the user pool unless we ignore this bit
+      schema
+    ]
+  }
+  email_configuration {
+    source_arn = "${var.identity_email}"
+    from_email_address = "${var.from_name} <${var.identity_email}>}"
+    email_sending_account = "DEVELOPER"
+  }
+}
+
+# App Client
+resource "aws_cognito_user_pool_client" "pool_client" {
+  name            = var.clientname
+  user_pool_id    = aws_cognito_user_pool.user_pool.id
+  generate_secret = false
+
+  ### LEGACY or ENABLED (AWSの推奨はENABLED)
+  prevent_user_existence_errors = "ENABLED"
+
+}
+
+data "aws_ses_email_identity" "example" {
+  email = var.identity_email
+}
+
+data "aws_iam_policy_document" "example" {
+  statement {
+    actions   = ["SES:SendEmail"]
+    resources = [aws_ses_email_identity.example.arn]
+
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+  }
+}
+
+resource "aws_ses_identity_policy" "example" {
+  identity = data.aws_ses_email_identity.example.arn
+  name     = var.identity_name
+  policy   = data.aws_iam_policy_document.example.json
+}
---
+++ test_cognito/variable.tf
@@ -0,0 +1,13 @@
+variable "poolname" {}
+variable "clientname" {}
+variable "schemas" {}
+variable "auto_verified" {}
+variable "create_user" {}
+variable "email_subject" {}
+variable "email_message" {}
+variable "account_recovery" {}
+variable "mfa_configuration" {}
+variable "name" {}
+variable "identity_email" {}
+variable "identity_name" {}
+variable "from_name" {}
