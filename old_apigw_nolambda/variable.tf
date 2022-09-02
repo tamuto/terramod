fbea3e166c610b3d57951c106b32f7c6dbf62a25
tagaku0510 <yuuki.tagaku@abi-inc.co.jp>
2022-09-02 05:51:13 +0000

    CognitoAuthrization付API G/W構成追加

--- old_apigw_nolambda/variable.tf
+++ old_apigw_nolambda/variable.tf
@@ -1,8 +1,8 @@
 variable "name" {}
-# variable "cognito_user_client_id" {}
-# variable "cognito_user_pool_id" {}
-# variable "cognito_manager_client_id" {}
-# variable "cognito_manager_pool_id" {}
+variable "cognito_user_client_id" {}
+variable "cognito_user_pool_id" {}
+variable "cognito_manager_client_id" {}
+variable "cognito_manager_pool_id" {}
 variable "allow_headers" {
     default = ["Content-Type", "X-Amz-Date", "Authorization", "X-Api-Key", "X-Amz-Security-Token"]
 }
@@ -11,5 +11,5 @@ variable "allow_methods" {
     default = ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }
 variable "allow_origins" {}
-# variable "user_poolname" {}
-# variable "manager_poolname" {}
+variable "user_poolname" {}
+variable "manager_poolname" {}
