3dcdf55ba39ba3e71850148ad67419351991a880
tagaku0510 <yuuki.tagaku@abi-inc.co.jp>
2022-09-02 05:42:32 +0000

    CognitoAuthrization付API G/W構成追加

--- old_apigw_nolambda/cloudwatch.tf
+++ old_apigw_nolambda/cloudwatch.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_log_group" "apigw" {
-    name = "/aws/apigateway/apigw"
-    retention_in_days = 7
-}
+# resource "aws_cloudwatch_log_group" "apigw" {
+#     name = "/aws/apigateway/apigw"
+#     retention_in_days = 7
+# }
