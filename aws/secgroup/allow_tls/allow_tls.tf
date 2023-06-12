41190f40b97182623350be2368db7248adccdbd7
tamuto <mutomob@gmail.com>
2023-06-12 23:09:01 +0000

    Outboundルールを変更

--- aws/secgroup/allow_tls/allow_tls.tf
+++ aws/secgroup/allow_tls/allow_tls.tf
@@ -11,9 +11,9 @@ resource "aws_security_group" "allow_tls" {
     }
 
     egress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
+        from_port = 0
+        to_port = 0
+        protocol = -1
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
