1bd54d2e8c8e0ad56a249ccf3366f1319284e1ff
tamuto <mutomob@gmail.com>
2023-06-22 00:18:12 +0000

    ポリシー生成

--- aws/role/lambda/main.tf
+++ aws/role/lambda/main.tf
@@ -2,7 +2,14 @@ resource "aws_iam_role" "lambda" {
     name = var.role_name
     path = var.role_path
     assume_role_policy = data.aws_iam_policy_document.lambda_policy.json
-    managed_policy_arns = data.aws_iam_policy_document.logging_policy.json
+    managed_policy_arns = [ aws_iam_policy.lambda.arn ]
+}
+
+resource "aws_iam_policy" "lambda" {
+    name = "${var.role_name}-policy"
+    path = var.role_path
+
+    policy = data.aws_iam_policy_document.logging_policy.json
 }
 
 data "aws_caller_identity" "current" {}
